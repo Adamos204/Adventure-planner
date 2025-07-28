3e55bc696b23cd6a6bb91c915e4faa4194c1cd17
Adamos204 <adam.a.vesely@gmail.com>
2025-07-28 22:59:54 +0200

    Initial commit

---
+++ backend/src/main/java/org/example/adventure_planner/AdventurePlannerApplication.java
@@ -0,0 +1,13 @@
+package org.example.adventure_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventurePlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventurePlannerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/adventure_planner/AdventurePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.adventure_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurePlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
